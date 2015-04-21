acc8f67ebe1f67677fc7cdd4cd2a961e1f5556c3
Bram Vogelaar <attachmentgenie@inuits.eu>
2015-04-21 15:19:40 +0200

    fixing foreman reporting from compile masters

--- manifests/profile_foreman_proxy.pp
+++ manifests/profile_foreman_proxy.pp
@@ -3,7 +3,7 @@ class profile_foreman_proxy (
   $puppetca     = false,
 ) {
   class { '::foreman_proxy':
-    foreman_base_url => "http://${$foreman_host}",
+    foreman_base_url => "http://${foreman_host}",
     trusted_hosts    => [$::fqdn, $foreman_host],
     bmc              => false,
     dhcp             => false,
