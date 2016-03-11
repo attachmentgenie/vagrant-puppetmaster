df639f93ecd4416e7c372e385138f39f99ace83f
Bram Vogelaar <attachmentgenie@inuits.eu>
2016-03-11 22:38:24 +0100

    updating submodules to latest usable version, i am looking at you puppetdb

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,3 @@
-$role = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.100']
-$tier = $trusted['extensions']['1.3.6.1.4.1.34380.1.1.101']
-
 hiera_include('classes',['puppetmaster::role_node'])
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
